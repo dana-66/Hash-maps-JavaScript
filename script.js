e99f020de466c303d0f383d8dce0f8ae8339d57c
dana-66 <danatarik66@gmail.com>
2024-08-26 03:18:38 +0300

    set method

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//constructor function for Map Object
 let maps = new Map ([
 	['kabc', 1],
 	['kdef', 2],
@@ -6,3 +7,11 @@ let maps = new Map ([
 
 console.log(maps);
 //map { 'kabc': 1, 'kdef': 2, 'gei': 3}
+
+let mapHash = new Map ();
+mapHash.set('Precious','german shepherd');
+mapHash.set('Sushi','Poodle');
+mapHash.set('Petunia','retriever');
+mapHash.set('lilly','husky');
+
+console.log(mapHash.get('Sushi'));
